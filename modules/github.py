7053d3a3803b60e07c1744f2c2f13b8f05af4143
whocodeswhat <mehtamishthi2@gmail.com>
2026-07-02 23:56:46 +0530

    added website and profile url

--- modules/github.py
+++ modules/github.py
@@ -18,26 +18,35 @@ def check_github(username):
             followers=page.locator("a[href$='?tab=followers'] span").inner_text()
             github_data["result"]="FOUND"
             github_data["name"]=name
+            
             if page.locator(".p-note").is_visible():
                 github_data["bio"]=page.locator(".p-note").inner_text()
             else:
-                github_data["bio"]="None"
+                github_data["bio"] = None
             github_data["followers"]=followers
             github_data["following"]=page.locator("a[href$='?tab=following'] span").inner_text()
             github_data["repositories"]=page.locator("a[href$='?tab=repositories'] span").first.inner_text()
+            
             if page.locator(".p-org").is_visible():
                 github_data["company"]=page.locator(".p-org").inner_text()
             else:
-                github_data["company"]="None"
+                github_data["company"] = None
             
             if page.locator(".p-label").is_visible():
                 github_data["location"]=page.locator(".p-label").inner_text()
             else:
-                       github_data["location"]="None"
+                       github_data["location"] = None
+
+            website = page.locator('a[rel~="nofollow"]')
+            if website.count() > 0:
+                   github_data["website"] = website.get_attribute("href")
+            else:
+                     github_data["website"] = None
+
+            github_data["profile_url"] = url    
      
-            page.screenshot(path=f"screenshots/{username}.png")    
+            page.screenshot(path=f"screenshots/{username}.png")
 
         browser.close()
 
-    return github_data 
-
+    return github_data
